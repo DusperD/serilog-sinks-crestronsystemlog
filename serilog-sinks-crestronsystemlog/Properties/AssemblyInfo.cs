4b0111d1062e9d405a37e6412660f95489d228d8
Dario Dusper <dario.dusper@gmx.at>
2021-12-14 20:56:45 +0100

    cahnges

--- serilog-sinks-crestronsystemlog/Properties/AssemblyInfo.cs
+++ serilog-sinks-crestronsystemlog/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("serilog-sinks-crestronsystemlog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Serilog-Sinks-Crestronsystemlog")]
+[assembly: AssemblyDescription("Write to Crestron System Log")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dario Dusper")]
 [assembly: AssemblyProduct("serilog-sinks-crestronsystemlog")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
