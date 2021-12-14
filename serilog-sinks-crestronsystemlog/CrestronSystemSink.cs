c43342ac59ae39606a3875904b670eef1189a2ba
Dario Dusper <dario.dusper@gmx.at>
2021-12-14 13:13:02 +0100

    N/A

---
+++ serilog-sinks-crestronsystemlog/CrestronSystemSink.cs
@@ -0,0 +1,61 @@
+﻿using Crestron.SimplSharp;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting;
+using System;
+
+namespace serilog_sinks_crestronsystemlog
+{
+    public class CrestronSystemSink : ILogEventSink
+    {
+        private readonly ITextFormatter _formatter;
+
+        public CrestronSystemSink(ITextFormatter formatter)
+        {
+            _formatter = formatter ?? throw new ArgumentNullException(nameof(formatter));
+        }
+        public void Emit(LogEvent logEvent)
+        {
+            using (var buffer = new System.IO.StringWriter())
+            {
+                _formatter.Format(logEvent, buffer);
+                if (logEvent.Level == LogEventLevel.Debug)
+                {
+                    ErrorLog.Notice(buffer.ToString());
+
+                }
+                else if (logEvent.Level == LogEventLevel.Information)
+                {
+                    ErrorLog.Notice(buffer.ToString());
+
+                }
+                else if (logEvent.Level == LogEventLevel.Warning)
+                {
+                    ErrorLog.Warn(buffer.ToString());
+
+                }
+                else if (logEvent.Level == LogEventLevel.Error)
+                {
+                    ErrorLog.Error(buffer.ToString());
+
+                }
+                else if (logEvent.Level == LogEventLevel.Fatal)
+                {
+                    ErrorLog.Error(buffer.ToString());
+
+                }
+                else if (logEvent.Level == LogEventLevel.Verbose)
+                {
+                    ErrorLog.Error(buffer.ToString());
+
+                }
+
+            }
+
+
+
+
+
+        }
+    }
+}
--- serilog-sinks-crestronsystemlog/CrestronSystemSinkExtension.cs
+++ serilog-sinks-crestronsystemlog/CrestronSystemSinkExtension.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Serilog;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Display;
+using System;
 
 namespace serilog_sinks_crestronsystemlog
 {
-    public class CrestronSystemSinkExtension
+    // <summary>
+    /// Adds the WriteTo.CrestronSystemSink() extension method 
+    /// </summary>
+    public static class CrestronSystemSinkExtension
     {
+        const string DefaultDebugOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+        /// <summary>
+        /// Writes log events to Crestron System Log/>.
+        /// </summary>
+        /// <param name="loggerConfiguration">Logger sink configuration.</param>
+        /// <param name="restrictedToMinimumLevel">The minimum level forevents passed through the sink. Ignored when <paramref name="levelSwitch"/> is specified.</param>
+        /// <param name="levelSwitch">A switch allowing the pass-through minimum level to be changed at runtime.</param>
+        /// <param name="outputTemplate">A message template describing the format used to write to the sink.the default is <code>"[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}"</code>.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <returns>Configuration object allowing method chaining.</returns>
+
+        public static LoggerConfiguration CrestronSystemSink(
+              this LoggerSinkConfiguration loggerConfiguration,
+              LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
+              string outputTemplate = DefaultDebugOutputTemplate,
+              IFormatProvider formatProvider = null,
+              LoggingLevelSwitch levelSwitch = null)
+        {
+
+            if (loggerConfiguration == null) throw new ArgumentNullException(nameof(loggerConfiguration));
+            if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+
+            var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
+            return loggerConfiguration.Sink(new CrestronSystemSink(formatter));
+        }
     }
 }
